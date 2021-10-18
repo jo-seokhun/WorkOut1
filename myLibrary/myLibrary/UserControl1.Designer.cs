12c3db6c353fba30799bcb6e0a487bd72d687030
jo-seokhun <88314920+jo-seokhun@users.noreply.github.com>
2021-10-18 09:42:38 +0900

    Add files via upload

---
+++ myLibrary/myLibrary/UserControl1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace myLibrary
+{
+    partial class frminput
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Prompt = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Prompt
+            // 
+            this.Prompt.AutoSize = true;
+            this.Prompt.Font = new System.Drawing.Font("Consolas", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Prompt.Location = new System.Drawing.Point(12, 7);
+            this.Prompt.Name = "Prompt";
+            this.Prompt.Size = new System.Drawing.Size(54, 20);
+            this.Prompt.TabIndex = 0;
+            this.Prompt.Text = "Input";
+            this.Prompt.Visible = false;
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(15, 30);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(172, 25);
+            this.tbInput.TabIndex = 1;
+            this.tbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbInput_KeyDown);
+            // 
+            // frminput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 67);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.Prompt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frminput";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Prompt;
+        private System.Windows.Forms.TextBox tbInput;
+    }
+}
---
+++ myLibrary/myLibrary/UserControl1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Data.SqlClient;
+
+namespace myLibrary
+{
+    public partial class frminput: Form
+    {
+        public frminput()
+        {
+            InitializeComponent();
+        }
+        public frminput(string str)
+        {
+            InitializeComponent();
+            Prompt.Text = str;
+            Prompt.Visible = true;
+        }
+        public frminput(int i)
+        {
+            InitializeComponent();
+            Prompt.Text = i.ToString();
+            Prompt.Visible = true;
+        }
+        public string retStr="";
+        private void tbInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                DialogResult = DialogResult.OK;
+                retStr = tbInput.Text;
+                Close();
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                DialogResult = DialogResult.Cancel;
+                retStr = "";
+
+                Close();
+            }
+            
+        }
+
+        
+
+
+
+
+        public class iniClass
+        {
+            string iniPath;
+
+            [DllImport("kernel32.dll")]
+            static extern int GetPrivateProfileString(string sec, string key, string def, StringBuilder buf, int bsize, string path);
+
+            [DllImport("kernel32.dll")]
+            static extern bool WritePrivateProfileString(string sec, string key, string val, string path);
+
+            public iniClass(string path)
+            {
+                iniPath = path;
+            }
+
+            public string GetString(string sec, string key)
+            {
+                StringBuilder buf = new StringBuilder(512);
+
+                GetPrivateProfileString(sec, key, " ", buf, 512, iniPath);
+                return buf.ToString();
+            }
+
+            public string GetString(string sec, string key, string def)
+            {
+                StringBuilder buf = new StringBuilder(512);
+
+                GetPrivateProfileString(sec, key, def, buf, 512, iniPath);
+                return buf.ToString();
+            }
+
+            public bool WriteString(string sec, string key, string val)
+            {
+                return WritePrivateProfileString(sec, key, val, iniPath);
+            }
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+
+    public static class mylib
+    {
+
+        //GetToken함수_ex) GetToken(1,"a,b,c,d",',')은 a 출력
+
+        public static string GetToken(int n, string str, char d)
+        {
+            string[] sarr = str.Split(d);
+            if (n < sarr.Length) return sarr[n];
+            return "";
+        }
+        public static string GetTokenEX(int n, string str, char d)
+        {
+            int i, j, k, n1, n2;
+            for (i = j = k = n1 = n2 = 0; i < str.Length; i++)
+            {
+                if (str[i] == d) k++;
+                if (k == n) n1 = i;
+                if (k == n + 1) n2 = i;
+            }
+            if (n1 == 0) return "";
+            if (n2 == 0) n2 = str.Length + 1;
+
+            return str.Substring(n1, n2 - 1 - n1);
+        }
+        public static string GetInput(string str)
+        {
+            frminput dlg = new frminput(str);
+            dlg.ShowDialog();
+            return dlg.retStr;
+        }
+
+
+    }
+
+    public class iniFile
+    {
+        [DllImport("kernel32.dll")]
+        static extern int GetPrivateProfileString(string s, string k, string d, StringBuilder b, int n, string p);
+        [DllImport("kernel32.dll")]
+        static extern bool WritePrivateProfileString(string s, string k, string v, string p);
+
+        public string fPath;
+        public iniFile(string str)
+        {
+            fPath = str;
+        }
+        public string GetString(string sec, string key, string def = "")
+        {
+            StringBuilder buf = new StringBuilder(512);
+            GetPrivateProfileString(sec, key, def, buf, 512, fPath);
+            return buf.ToString();
+        }
+        public bool WriteString(string sec, string key, string val)
+        {
+            return WritePrivateProfileString(sec, key, val, fPath);
+        }
+
+    }
+
+    public class SqlDB
+    {
+        SqlConnection conn = null;
+        SqlCommand cmd = null;
+        string ConnString = null;
+
+        public SqlDB(string str)
+        {
+            ConnString = str;
+            conn = new SqlConnection(ConnString);
+            conn.Open();
+            cmd = new SqlCommand("", conn);
+        }
+
+        public object Run(string sql)
+        {
+            try
+            {
+                char[] ca = { ' ', '\t', '\r', '\n' };
+                cmd.CommandText = sql;
+                //"Select * from table"
+                string s = sql.Trim().Split(ca)[0].ToLower();
+                if (s == "select")
+                {
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    DataTable dt = new DataTable();             //2차원 Array와 비슷한 놈 
+                    dt.Load(sdr);
+                    sdr.Close();
+                    return dt;
+                }
+                else
+                {
+                    return cmd.ExecuteNonQuery();   //int
+                }
+
+            }
+            catch(Exception e)
+            {
+                return null;
+            }
+
+        }
+
+        public string GetString(string sql)     // 단일 데이터 (1st record  1st field)
+        {       //select name from users where name = "Noname"
+            try
+            {
+                cmd.CommandText = sql;
+                return cmd.ExecuteScalar().ToString();
+
+            }
+
+            catch(Exception e1)
+            {
+                return e1.Message;
+            }
+        }
+
+    }
+
+
+
+
+}
